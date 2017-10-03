c7639335e475945748eb368075dbe170439e18e5
rogerioesilva <professor.rogerio@gmail.com>
2017-10-03 01:14:53 -0300

    Realce

--- Python/histograma.py
+++ Python/histograma.py
@@ -3,14 +3,18 @@ import scipy.misc
 import matplotlib.pyplot as plt
 
 path = "imagens/"
-imagem = scipy.misc.imread(path+"lena.jpg")
+imagem = scipy.misc.imread(path+"car1.png")
 
 # gera o histograma e seus intervalos
 histograma, intervalos = np.histogram(imagem,bins=np.arange(0,256))
 
+#print histograma
+#print intervalos
+
 # determina o valor do centro dos intervalos
-center = (intervalos[:-1] + intervalos[1:])/2
+#center = (intervalos[:-1] + intervalos[1:])/2
+#print center
 
 # exibe o histograma
-plt.bar(center, histograma, align='center')
+plt.bar(intervalos[:-1],histograma,1)
 plt.show()
